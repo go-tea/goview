51b6c3e2b5a6f4f60c3ea9fd1726a203984e8758
Andrea <vetrovskaandrea@gmail.com>
2019-04-30 12:30:51 +0200

    add supports/bindata

---
+++ supports/bindata/assetfs.go
@@ -0,0 +1,34 @@
+package bindata
+
+import (
+	"github.com/elazarl/go-bindata-assetfs"
+	"github.com/foolin/goview"
+)
+
+// go-bindata -prefix "dir/" -pkg main -o bindata.go dir/...
+
+/**
+New template engine, default views root.
+*/
+func New(viewsRootBox *assetfs.AssetFS) *goview.ViewEngine {
+	return NewWithConfig(viewsRootBox, goview.DefaultConfig)
+}
+
+func NewWithConfig(viewsRootBox *assetfs.AssetFS, config goview.Config) *goview.ViewEngine {
+	config.Root = viewsRootBox.Prefix
+	engine := goview.New(config)
+	engine.SetFileHandler(FileHandler(viewsRootBox))
+	return engine
+}
+
+func FileHandler(viewsRootBox *assetfs.AssetFS) goview.FileHandler {
+	return func(config goview.Config, tplFile string) (content string, err error) {
+		// get file contents as string
+		filename := tplFile + config.Extension
+		data, err := viewsRootBox.Asset(filename)
+		if err != nil {
+			return "", err
+		}
+		return string(data), nil
+	}
+}
